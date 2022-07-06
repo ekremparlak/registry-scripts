91fde6fded37797cb0008d3cff1693cdaa3c850b
Ekrem Parlak <ekremparlak@outlook.com>
2022-07-06 18:29:11 +0000

    add removetags.sh

--- Dockerfile
+++ Dockerfile
@@ -3,4 +3,4 @@ RUN apt-get update && apt-get install -y \
     ca-certificates \
     && rm -rf /var/lib/apt/lists/*
 COPY --from=regclient/regctl /regctl /usr/bin/regctl
-COPY ./scripts/keeptags.sh /usr/bin/keeptags
+COPY ./scripts/* /usr/bin/
--- scripts/removetags.sh
+++ scripts/removetags.sh
@@ -9,15 +9,15 @@ set -eu
 IFS=', ' read -r -a PREFIX <<< "$PREFIX"
 
 checkprefix(){
-    MATCHES=0
     for i in ${PREFIX[@]}; do
         if [[ $line == $i ]]; 
         then {
-            MATCHES=1
+            removetags
+            return 0
         }
         fi
     done
-    return $MATCHES
+    return 0
 }
 
 removetags() {
@@ -25,6 +25,6 @@ removetags() {
     regctl tag delete $REGISTRY/$REGISTRY_REPO:$line
 }
 
-regctl tag ls $REGISTRY/$REGISTRY_REPO | while read line ; do checkprefix && removetags; done
+regctl tag ls $REGISTRY/$REGISTRY_REPO | while read line ; do checkprefix; done
 
 exit 0
